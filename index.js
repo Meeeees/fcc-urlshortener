8a81978cde3649330f8bb205d604ba3c1c4655a9
meeeeesgithub <119116083+meeeeesgithub@users.noreply.github.com>
2023-07-18 15:56:09 +0200

    Initial commit

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log('DB Connected!'))
+
+const { Schema } = mongoose;
+const urlSchema = new Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: Number, required: true },
+});
+
+const Url = mongoose.model('Url', urlSchema);
 
 app.use(cors());
 
@@ -18,6 +35,18 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', async (req, res) => {
+  const { url } = req.body;
+  const urlRegex = /^(http|https):\/\/[^ "]+$/;
+  if (!urlRegex.test(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+  const urlCount = await Url.countDocuments();
+  const newUrl = new Url({ original_url: url, short_url: urlCount + 1 });
+  await newUrl.save();
+  return res.json({ original_url: url, short_url: urlCount + 1 });
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
